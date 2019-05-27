6ee1ef15a2e8dcd7647f0a9cc579cbd7a0f6d61e
$uicideCode$ <47318764+FaustinGarreau@users.noreply.github.com>
2019-05-27 14:58:49 +0200

    Modification

--- js/project.js
+++ js/project.js
@@ -1,10 +1,21 @@
 var xmlhttp = new XMLHttpRequest();
 var url = "https://private-fabb9f-xd4.apiary-mock.com/element";
+
 xmlhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
+
         var myArr = JSON.parse(this.responseText);
-        myFunction(myArr);
+        console.log(myArr);
+                                                                                
+        document.getElementById("bloc_titre").innerHTML = myArr.section[0].titre;
+        document.getElementById("bloc_titre2").innerHTML = myArr.section[1].titre;
+        document.getElementById("bloc_titre3").innerHTML = myArr.section[2].titre;
+        document.getElementById("text_bloc").innerHTML = myArr.section[0].texte;
+        document.getElementById("text_bloc2").innerHTML = myArr.section[1].texte;
+        document.getElementById("text_bloc3").innerHTML = myArr.section[2].texte;
     }
 };
 xmlhttp.open("GET", url , true);
 xmlhttp.send();
+
+	
